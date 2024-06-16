5eb7ec2cbbdf04b1dffa0be248e261e9b87d6d20
Mehul Chopra <mchopra@abico.com>
2024-06-16 17:03:18 +0530

    added default values and corrected the formula

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,5 @@
-def perimeter(length, breadth):
-  # deliberately adding this wrong formula
-  return 3 * (length + breadth)
+def perimeter(length, breadth=2):
+  return 2 * (length + breadth)
 
 def area(length, breadth):
   return length * breadth
